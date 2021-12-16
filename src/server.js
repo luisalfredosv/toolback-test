b7028a5821121ed92066769235e6156c1d5499ee
luisalfredosv <luissarabia80@gmail.com>
2021-12-16 15:59:33 -0400

    [update] Rename from index.js to server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 
+import routes from "./routes/index";
+
 const app = express();
 const port = 3000;
 
@@ -10,6 +12,8 @@ app.use(cors());
 app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
+
+app.use("/iecho", routes.iechoRoutes);
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
