df9b7656f34660f33c812ac0a7795c2a78c053cf
luisalfredosv <luissarabia80@gmail.com>
2021-12-19 11:41:56 -0400

    [create] Integration test /iecho

---
+++ src/tests/iecho.test.js
@@ -0,0 +1,22 @@
+import supertest from "supertest";
+import { app, server } from "../../server";
+
+const api = supertest(app);
+
+describe("Integration Test", () => {
+	test("GET /iecho return text reverse and return is a palindrome", async () => {
+		const response = await api
+			.get("/iecho?text=sol")
+			.expect(200)
+			.expect("Content-Type", /application\/json/);
+	});
+
+	test("GET /iecho return error 500 not text", async () => {
+		const response = await api
+			.get("/iecho?text=")
+			.expect(400)
+			.expect("Content-Type", /application\/json/);
+	});
+});
+
+afterAll(() => server.close());
