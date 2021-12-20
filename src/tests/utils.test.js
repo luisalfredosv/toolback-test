c437175853b33fd7895ef35662113b18249715b1
luisalfredosv <luissarabia80@gmail.com>
2021-12-19 20:15:33 -0400

    [create] Utils test

---
+++ src/tests/utils.test.js
@@ -0,0 +1,27 @@
+import { isPalindrome, reverseText } from "../utils/index";
+
+describe("Unit Test", () => {
+	test("Check is palindrome, return false", () => {
+		const text = "test";
+		const reverse = "tset";
+
+		const response = isPalindrome(reverse, text);
+		expect(response).toEqual(false);
+	});
+
+	test("Check is palindrome, return true", () => {
+		const text = "oso";
+		const reverse = "oso";
+
+		const response = isPalindrome(reverse, text);
+		expect(response).toEqual(true);
+	});
+
+	test("Reverse text", () => {
+		const text = "Hi, how are you?";
+		const reverse = "?uoy era woh ,iH";
+
+		const response = reverseText(text);
+		expect(response).toEqual(reverse);
+	});
+});
